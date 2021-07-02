cb1b24525cf604f789ec44c7a723f8e3f84e03ad
leandroalvesmachado@gmail.com <leandroalvesmachado@gmail.com>
2021-07-01 22:38:08 -0300

    Iniciando Aula 03 Cap 06

--- php-e-pdo-trabalhando-com-banco-de-dados/php-pdo-projeto-inicial/inserir-aluno.php
+++ php-e-pdo-trabalhando-com-banco-de-dados/php-pdo-projeto-inicial/inserir-aluno.php
@@ -7,17 +7,34 @@ require_once 'vendor/autoload.php';
 $databasePath = __DIR__ . '/banco.sqlite';
 $pdo = new PDO('sqlite:' . $databasePath);
 
-$student = new Student(null, 'Vinicius Dias', new \DateTimeImmutable('1997-10-15'));
+$student = new Student(
+    null,
+    'Vinicius Dias 2',
+    new \DateTimeImmutable('1997-10-15')
+);
 
-$sqlInsert = "
-    INSERT INTO students (name, birth_data) 
-    VALUES('{$student->name()}', '{$student->birthDate()->format('Y-m-d')}')
-";
-
-echo $sqlInsert;
+// $sqlInsert = "INSERT INTO students (name, birth_date) VALUES ('{$student->name()}', '{$student->birthDate()->format('Y-m-d')}');";
+// echo $sqlInsert;
 
 // se exec retornar 1 (inteiro) funcionou
 // caso o exec for utilizado para uma consulta, ele retorna a qtd de registros da consulta
-$pdf->exec($sqlInsert);
+// var_dump($pdo->exec($sqlInsert));
+
+// criando insert mais seguro
+$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (?, ?);";
+$statement = $pdo->prepare($sqlInsert);
+$statement->bindValue(1, $student->name());
+$statement->bindValue(2, $student->birthDate()->format('Y-m-d'));
+
+echo $sqlInsert;
+
+if ($statement->execute()) {
+    echo 'Aluno salvo';
+}
+
+
+
+exit();
+
 
 
---
+++ php-e-pdo-trabalhando-com-banco-de-dados/php-pdo-projeto-inicial/lista-alunos.php
@@ -0,0 +1,52 @@
+<?php
+// https://www.php.net/manual/pt_BR/pdostatement.fetchall.php
+// Ao tentar buscar dados do banco de dados, você pode informar como quer que os métodos do PDO formatem esses dados pra você. 
+// Os principais fetch modes ou fetch styles são:
+
+// PDO::FETCH_ASSOC: Retorna cada linha como um array associativo, onde a chave é o nome da coluna, e o valor é o valor da coluna em si
+// PDO::FETCH_BOTH (esse é o padrão): Retorna cada linha como um array com as chaves sendo tanto o índice da coluna (começando do 0) quanto o nome da coluna, ou seja, os valores acabam ficando duplicados nesse formato
+// PDO::FETCH_CLASS: Cada linha do resultado é retornado como uma instância da classe especificada em outro parâmetro. A classe não pode ter parâmetros no construtor e cada coluna terá o seu valor atribuído a uma propriedade de mesmo nome no objeto criado
+// PDO::FETCH_INTO: Funciona de forma muito semelhante ao FETCH_CLASS, mas ao invés de criar o objeto para você, ele preenche um objeto já criado com os valores buscados
+// PDO::FETCH_NUM: Retorna cada linha como um array, onde a chave é o índice numérico da coluna, começando do 0, e o valor é o valor da coluna em si
+// Para ver os demais modos de busca e ler com mais detalhes os explicados aqui, você pode conferir a documentação oficial do PHP: PDOStatement::fetch.
+
+use Alura\Pdo\Domain\Model\Student;
+
+require_once 'vendor/autoload.php';
+
+$databasePath = __DIR__ . '/banco.sqlite';
+$pdo = new PDO('sqlite:' . $databasePath);
+
+$statement = $pdo->query("SELECT * FROM students;");
+
+
+$studentDataList = $statement->fetchAll(PDO::FETCH_ASSOC);
+$studentList = [];
+// var_dump($studentDataList);
+
+foreach ($studentDataList as $studentData) {
+    $studentList[] = new Student(
+        $studentData['id'],
+        $studentData['name'],
+        new \DateTimeImmutable($studentData['birth_date'])
+    );
+}
+
+// var_dump($studentList);
+
+
+// retornando apenas 1 registro
+$statement = $pdo->query("SELECT * FROM students WHERE id = 1;");
+// $studentData = $statement->fetch(PDO::FETCH_ASSOC);
+// var_dump($studentData);
+
+// outra forma
+while ($studentData = $statement->fetch(PDO::FETCH_ASSOC)) {
+    $student = new Student(
+        $studentData['id'],
+        $studentData['name'],
+        new \DateTimeImmutable($studentData['birth_date'])
+    );
+
+    echo $student->age() . PHP_EOL;
+}
