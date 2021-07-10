fd4f30210539c2d683e2d643fb129b8429a0227d
leandroalvesmachado@gmail.com <leandroalvesmachado@gmail.com>
2021-07-10 11:21:26 -0300

    Fim

--- php-e-pdo-trabalhando-com-banco-de-dados/php-pdo-projeto-inicial/conexao.php
+++ php-e-pdo-trabalhando-com-banco-de-dados/php-pdo-projeto-inicial/conexao.php
@@ -6,11 +6,12 @@ $pdo = new PDO('sqlite:' . $caminhoBanco);
 echo 'Conectado';
 
 $pdo->exec(
-    "INSERT INTO phones (area_code, number, student_id) VALUES ('24', '9999999999', 1), ('85', '8888888888', 1)"
+    "INSERT INTO phones (area_code, number, student_id) VALUES ('24', '999999999', 1), ('85', '888888888', 1)"
 );
 
 exit();
 
+// sqlite
 $pdo->exec('
     CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY,
@@ -26,3 +27,20 @@ $pdo->exec('
         FOREIGN KEY(student_id) REFERENCES students (id)
     );
 ');
+
+// mysql
+// $pdo->exec('
+//     CREATE TABLE IF NOT EXISTS students (
+//         id INTEGER PRIMARY KEY AUTO_INCREMENT,
+//         name VARCHAR(128),
+//         birth_date DATE
+//     );
+
+//     CREATE TABLE IF NOT EXISTS phones (
+//         id INTEGER PRIMARY KEY AUTO_INCREMENT,
+//         area_code CHAR(2),
+//         number CHAR(9),
+//         student_id INTEGER,
+//         FOREIGN KEY(student_id) REFERENCES students (id)
+//     );
+// ');
--- php-e-pdo-trabalhando-com-banco-de-dados/php-pdo-projeto-inicial/src/Infrastructure/Persistence/ConnectionCreator.php
+++ php-e-pdo-trabalhando-com-banco-de-dados/php-pdo-projeto-inicial/src/Infrastructure/Persistence/ConnectionCreator.php
@@ -6,10 +6,25 @@ use PDO;
 
 class ConnectionCreator
 {
+    // sqlite
+    // public static function createConnection(): PDO
+    // {
+    //     $databasePath = __DIR__ . '/../../../banco.sqlite';
+    //     $connection = new PDO('sqlite:' . $databasePath);
+    //     // habilita pdo a lancar as excecoes, por padrao vem silencioso
+    //     // PDO::ATTR_ERRMODE existem outros modos
+    //     $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    //     // definindo o FETCH_ASSOC como retorno padrao das consultas
+    //     $connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+    //     return $connection;
+    // }
+
+    // mysql
     public static function createConnection(): PDO
     {
-        $databasePath = __DIR__ . '/../../../banco.sqlite';
-        $connection = new PDO('sqlite:' . $databasePath);
+        $connection = new PDO('mysql:host=localhost;dbname=alurapdo', 'root', '1q2w3e4r');
         // habilita pdo a lancar as excecoes, por padrao vem silencioso
         // PDO::ATTR_ERRMODE existem outros modos
         $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
